79f22e79646dc7f666b28a8aaf4973caad0cb895
David Hubner <david.hubner@gmail.com>
2016-03-23 23:25:35 +0100

    Login rate limit trait

---
+++ src/PhalconExt/Mvc/Model/Traits/RateLimitLoginTrait.php
@@ -0,0 +1,115 @@
+<?php
+
+/*
+ * Phalcon Ext
+ * Copyright (c) 2011-2016 David Hübner
+ * This source file is subject to the New BSD License that is bundled
+ * with this package in the file docs/LICENSE.
+ * Author: David Hübner <david.hubner@gmail.com>
+ */
+
+namespace PhalconExt\Mvc\Model\Traits;
+
+/**
+ * Adds login rate limit support to target model
+ * 
+ * Class using this trait must define 2 constants:
+ * 
+ *     MAX_FAILED_LOGIN_ATTEMPTS
+ *     ACCOUNT_LOCK_DURATION (in seconds)
+ *
+ * @author     David Hübner <david.hubner at google.com>
+ * @version    Release: @package_version@
+ * @since      Release 1.0
+ */
+trait RateLimitLoginTrait
+{
+
+    /**
+     * @var int $_failedLoginAttempts - failed login attempts 
+     */
+    private $_failedLoginAttempts = 0;
+
+    /**
+     * @var int $_failedLoginTs - last failed login timestamp
+     */
+    private $_failedLoginTs = 0;
+
+    /**
+     * @var int $lastLoginTs - last successfull login timestamp
+     */
+    public $lastLoginTs;
+
+    /**
+     * @var int $lastLoginIp - last successfull login IP address
+     */
+    public $lastLoginIp;
+
+    /**
+     * Returns actual failed login attempts
+     *
+     * @author  David Hübner <david.hubner at google.com>
+     * @return  int
+     */
+    public function getFailedLoginAttempts()
+    {
+        return $this->_failedLoginAttempts;
+    }
+
+    /**
+     * Checks wheather account is locked to prevent brute force attacs
+     *
+     * @author  David Hübner <david.hubner at google.com>
+     * @return  false | int - remaining lock duration
+     */
+    public function hasAccountLocked()
+    {
+        // not engough failed attempts
+        if ($this->_failedLoginAttempts < self::MAX_FAILED_LOGIN_ATTEMPTS) {
+            return false;
+        }
+
+        $diff = $this->_failedLoginTs + self::ACCOUNT_LOCK_DURATION - time();
+
+        // account lock in process
+        if ($diff > 0) {
+            return $diff;
+        }
+
+        // releasing lock
+        $this->_failedLoginAttempts = 0;
+        $this->_failedLoginTs = 0;
+
+        return false;
+    }
+
+    /**
+     * Increments failed login attempts
+     *
+     * @author  David Hübner <david.hubner at google.com>
+     * @return  bool
+     */
+    public function incrementFailedLoginAttempts()
+    {
+        $this->_failedLoginAttempts += 1;
+        $this->_failedLoginTs = time();
+        return $this->save();
+    }
+
+    /**
+     * Resets failed login attempts
+     *
+     * @author  David Hübner <david.hubner at google.com>
+     * @param   string $ipAddress - login IP address, default NULL
+     * @return  bool
+     */
+    public function resetFailedLoginAttempts($ipAddress = NULL)
+    {
+        $this->_failedLoginAttempts = 0;
+        $this->_failedLoginTs = 0;
+        $this->lastLoginTs = time();
+        $this->lastLoginIp = $ipAddress;
+        return $this->save();
+    }
+
+}
---
+++ tests/Mvc/Model/Traits/RateLimitLoginTraitTest.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace PhalconExt\Tests\Mvc\Model\Traits;
+
+use PhalconExt\Mvc\Model\Traits\RateLimitLoginTrait;
+
+class RateLimitLoginTraitTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var RateLimitLoginTraitMock
+     */
+    protected $object;
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->object = new RateLimitLoginTraitMock();
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+        
+    }
+
+    /**
+     * @covers PhalconExt\Mvc\Model\Traits\RateLimitLoginTrait::getFailedLoginAttempts
+     */
+    public function testGetFailedLoginAttempts()
+    {
+        $mock = $this->getMockForTrait('PhalconExt\Mvc\Model\Traits\RateLimitLoginTrait');
+        $this->assertSame(0, $mock->getFailedLoginAttempts());
+    }
+
+    /**
+     * @covers PhalconExt\Mvc\Model\Traits\RateLimitLoginTrait::getFailedLoginAttempts
+     * @covers PhalconExt\Mvc\Model\Traits\RateLimitLoginTrait::hasAccountLocked
+     * @covers PhalconExt\Mvc\Model\Traits\RateLimitLoginTrait::incrementFailedLoginAttempts
+     */
+    public function testAutoLock()
+    {
+        $this->assertSame(0, $this->object->getFailedLoginAttempts());
+        $this->assertFalse($this->object->hasAccountLocked());
+        $this->assertTrue($this->object->incrementFailedLoginAttempts());
+        $this->assertSame(1, $this->object->getFailedLoginAttempts());
+        $this->assertFalse($this->object->hasAccountLocked());
+        $this->assertTrue($this->object->incrementFailedLoginAttempts());
+        $this->assertSame(2, $this->object->getFailedLoginAttempts());
+        $this->assertFalse($this->object->hasAccountLocked());
+        $this->assertTrue($this->object->incrementFailedLoginAttempts());
+        $this->assertSame(3, $this->object->getFailedLoginAttempts());
+        $this->assertGreaterThan(0, $this->object->hasAccountLocked());
+    }
+
+    /**
+     * @covers PhalconExt\Mvc\Model\Traits\RateLimitLoginTrait::getFailedLoginAttempts
+     * @covers PhalconExt\Mvc\Model\Traits\RateLimitLoginTrait::hasAccountLocked
+     * @covers PhalconExt\Mvc\Model\Traits\RateLimitLoginTrait::incrementFailedLoginAttempts
+     */
+    public function testAutoRelease()
+    {
+        $this->assertFalse($this->object->hasAccountLocked());
+        $this->assertTrue($this->object->incrementFailedLoginAttempts());
+        $this->assertTrue($this->object->incrementFailedLoginAttempts());
+        $this->assertTrue($this->object->incrementFailedLoginAttempts());
+        $this->assertGreaterThan(0, $this->object->hasAccountLocked());
+        sleep(RateLimitLoginTraitMock::ACCOUNT_LOCK_DURATION + 1);
+        $this->assertFalse($this->object->hasAccountLocked());
+        $this->assertSame(0, $this->object->getFailedLoginAttempts());
+    }
+
+    /**
+     * @covers PhalconExt\Mvc\Model\Traits\RateLimitLoginTrait::getFailedLoginAttempts
+     * @covers PhalconExt\Mvc\Model\Traits\RateLimitLoginTrait::hasAccountLocked
+     * @covers PhalconExt\Mvc\Model\Traits\RateLimitLoginTrait::incrementFailedLoginAttempts
+     * @covers PhalconExt\Mvc\Model\Traits\RateLimitLoginTrait::resetFailedLoginAttempts
+     */
+    public function testResetLock()
+    {
+        $this->assertFalse($this->object->hasAccountLocked());
+        $this->assertTrue($this->object->incrementFailedLoginAttempts());
+        $this->assertTrue($this->object->incrementFailedLoginAttempts());
+        $this->assertTrue($this->object->incrementFailedLoginAttempts());
+        $this->assertGreaterThan(0, $this->object->hasAccountLocked());
+        $this->assertTrue($this->object->resetFailedLoginAttempts());
+        $this->assertFalse($this->object->hasAccountLocked());
+        $this->assertSame(0, $this->object->getFailedLoginAttempts());
+    }
+
+}
+
+class RateLimitLoginTraitMock
+{
+
+    use RateLimitLoginTrait;
+
+    const MAX_FAILED_LOGIN_ATTEMPTS = 3;
+    const ACCOUNT_LOCK_DURATION = 2;
+
+    public function save()
+    {
+        return true;
+    }
+
+}
